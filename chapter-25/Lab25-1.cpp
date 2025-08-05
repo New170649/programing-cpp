d831b2da810891e04bbe5b59db13f4750d500b99
New68444134 <Pootawon123@gmail.com>
2025-08-05 11:02:24 +0700

    Update Lab25-1.cpp

--- chapter-25/Lab25-1.cpp
+++ chapter-25/Lab25-1.cpp
@@ -1,1 +1,26 @@
-ggg
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+class Circle {
+	public:
+	double radius;
+	
+	Circle(double rr){
+		radius = rr;
+	}
+	
+	void findArea() {
+		cout << "Area of circle is " << (3.14 * radius * radius) << std::endl;
+	}
+	void perimeter() {
+		cout << "Perimeter of cicle is " << (2.0 * 3.14 * radius) << std::endl;
+	}
+};
+
+int main(){
+	Circle c(20);
+	
+	c.findArea();
+	c.perimeter();
+}
